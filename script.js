bb9d9f1d2a133b59c3c7a59d3c83e49e1d099a8c
crossfire3001 <obiwan2035@mail.ru>
2023-07-17 20:51:19 +0500

    working with arrays

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
 "use strict";
 
 // 1
+const heroes = [
+  "Jungle boy",
+  "El Capitano",
+  "The SuperX",
+  "Chemical Joe",
+  "Do little",
+  3933,
+];
+
+// удаление последнего элемента из нашего массива
+heroes.pop();
+
+console.log(heroes);
+
+// 2
+const gangsters = [
+  "Jungle boy",
+  "El Capitano",
+  "The SuperX",
+  "Chemical Joe",
+  "Do little",
+  3933,
+];
+
+// добавление элемента в конец массива
+gangsters.push("The BIG");
+
+console.log(gangsters);
+
+// 3
+const chemicalBrothers = [
+  "Jungle boy",
+  "El Capitano",
+  "The SuperX",
+  "Chemical Joe",
+  "Do little",
+  3933,
+];
+
+// перебор элементов массива
+for (let i = 0; i < chemicalBrothers.length; i++) {
+  console.log(chemicalBrothers[i]);
+}
+
+// 4
+const supers = [
+  "Jungle boy",
+  "El Capitano",
+  "The SuperX",
+  "Chemical Joe",
+  "Do little",
+  3933,
+];
+
+// перебор элементов массива
+for(let value of supers) {
+    console.log(value);
+}
